072b7982b5858362e9f2e2e3e7f18a2f028160e2
pavel203 <pavel.tarasov799@gmail.com>
2021-08-03 00:44:34 +0300

    commit

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -31,11 +31,13 @@ public class UserDaoImp implements UserDao {
    @Override
    public User getUserByCar(String model, int series) {
 
-      String HQL="FROM User u JOIN car c ON u.car_id=c.id WHERE c.model=: model and c.series =:series";
+      String HQL="FROM User u JOIN car c ON u.car_id=c.id WHERE c.model=:model and c.series =:series";
       //String HQL="FROM Car car LEFT OUTER JOIN FETCH car.user WHERE car.model=: paramModel and car.series =: paramSeries";
       //Car car = sessionFactory.getCurrentSession().createQuery(HQL, Car.class).setParameter("id", 1).uniqueResult();
-      Car car = sessionFactory.getCurrentSession().createQuery(HQL, Car.class).setParameter(model, series).uniqueResult();
-      return car.getUser();
+      Query query = sessionFactory.getCurrentSession().createQuery(HQL, Car.class);
+              query.setParameter("model", model);
+      query.setParameter("series", series);
+      return (User) query.getResultList().get(0);
    }
 
 }
